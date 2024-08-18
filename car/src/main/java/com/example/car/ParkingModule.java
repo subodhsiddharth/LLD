8992fd74184d731b8242e405867b6bf08d5b9221
SUBODH SIDDHARTH <65944635+subodhsiddharth@users.noreply.github.com>
2024-08-18 22:11:57 +0530

    Added parking Module LLD
    car paring system

---
+++ car/src/main/java/com/example/car/ParkingModule.java
@@ -0,0 +1,11 @@
+package com.example.ParkingModule;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingModule {
+	public static void main(String[] args) {
+
+	}
+
+}
